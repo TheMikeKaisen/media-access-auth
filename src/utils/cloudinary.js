88ce0fbec97c7282f204468dfd0a48c3e0571349
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-01-06 15:51:29 +0530

    file upload - cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import {v2 as cloudinary} from 'cloudinary'
+
+import fs from 'fs'
+
+
+// Cloudinary configuration          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+
+        // uploading the file on cloudinary
+        const response = await cloudinary.v2.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)    //remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
+
